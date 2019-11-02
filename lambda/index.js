2d635f8a95dcd4bc8932cae3a8ee34773740166d
sadomski-cli+2-at-143552087076 <sadomski@amazon.com>
2019-11-02 20:56:09 +0100

    v4

--- lambda/index.js
+++ lambda/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda v3!'),
+        body: JSON.stringify('Hello from Lambda v4!'),
     };
     return response;
 };
